8c4ab31d2cce215b26862e420387f4e09ca9a85b
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2017-01-31 02:36:41 -0500

    breadcrumbs done

--- Homework1/src/KinematicMotion.java
+++ Homework1/src/KinematicMotion.java
@@ -24,29 +24,54 @@ public class KinematicMotion extends PApplet {
         shape = new CustomShape(this, "customShape.png", w, h);
         s = new SteeringClass(this);
 
-        s.setPosition(new PVector(w/2,height-h/2));
+        s.setPosition(new PVector(2*w/3,height-2*h/2));
         s.setVelocity(new PVector(3.2f,0.0f));
         //s.setAcceleration(new PVector(0.02f,0.03f));
         s.setOrientation(radians(0));
         //s.setRotation(0.01f);
-
-        //breadCrumbs.add(new PVector());
-
     }
 
     public void draw()
     {
         background(255);
+        breadCrumbs.add(new PVector(s.getPosition().x, s.getPosition().y));
+
         shape.setOrientation(s.getOrientation());
         shape.drawCustomShape(s.getPosition().x,s.getPosition().y);
-        handleCollision();
+        drawBreadcrumbs();
+
+        specialHandleCollision();
+
         s.update(1);
+
     }
 
     public static void main(String[] args)
     {
         PApplet.main("KinematicMotion");
     }
+
+
+    public void specialHandleCollision()
+    {
+        float radius = (float) (Math.sqrt(Math.pow(w,2) + Math.pow(h,2)))/2;
+
+        float x = s.getPosition().x;
+        float y = s.getPosition().y;
+
+        if ((((x+radius)>width) && (s.getVelocity().x>0)) || ((x-radius)<0) && (s.getVelocity().x<0))
+        {
+            s.setOrientation(s.getOrientation() + radians(-90));
+            s.setVelocity(new PVector(s.getVelocity().y, -s.getVelocity().x));
+        }
+
+        if ((((y+radius)>height) && (s.getVelocity().y>0)) || ((y-radius)<0) && (s.getVelocity().y<0))
+        {
+            s.setOrientation(s.getOrientation() + radians(-90));
+            s.setVelocity(new PVector(s.getVelocity().y, -s.getVelocity().x));
+        }
+    }
+
 
     public void handleCollision()
     {
@@ -65,6 +90,19 @@ public class KinematicMotion extends PApplet {
     public PVector convolution(PVector a, PVector b)
     {
         return (new PVector(a.x * b.x, a.y * b.y));
+    }
+
+    public void drawBreadcrumbs()
+    {
+        //System.out.println(breadCrumbs.size());
+        for (int i=0; i<breadCrumbs.size(); i++)
+        {
+            if (i%10==0)
+            {
+                fill(random(0,255));
+                ellipse(breadCrumbs.get(i).x, breadCrumbs.get(i).y, 10, 10);
+            }
+        }
     }
 
 }
