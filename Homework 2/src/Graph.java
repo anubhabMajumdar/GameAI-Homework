cee24162ebbe0a83281b49d75a09947a6d519b36
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2017-03-04 16:21:55 -0500

    makeGraph() for world_graph

---
+++ Homework 2/src/Graph.java
@@ -0,0 +1,119 @@
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by anubhabmajumdar on 3/4/17.
+ */
+public class Graph {
+
+    HashMap g = new HashMap();
+
+    public StringBuffer readFile(String fileName)
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        try {
+            File file = new File(fileName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+                stringBuffer.append("\n");
+            }
+            fileReader.close();
+//            System.out.println("Contents of file:");
+//            System.out.println(stringBuffer.toString());
+
+        } catch (IOException e) {
+            System.out.println("Wrong filename");
+            e.printStackTrace();
+        }
+
+        return stringBuffer;
+    }
+
+    public void makeGraph(String fileName)
+    {
+        StringBuffer stringBuffer = readFile(fileName);
+        String toString = stringBuffer.toString();
+        String edges[] = toString.split("\\r?\\n");
+
+        for (int i=1; i<edges.length; i++)
+        {
+            String cur = edges[i];
+            String vals[] = cur.split("\\t");
+
+            if (vals.length == 3)
+            {
+                int fromNode = Integer.parseInt(vals[0]);
+                int toNode = Integer.parseInt(vals[1]);
+                float weight = Float.parseFloat(vals[2]);
+
+                ArrayList<Edge> outEdges;
+
+                if (g.containsKey(fromNode))
+                {
+                   outEdges  = (ArrayList<Edge>) g.get(fromNode);
+                }
+                else
+                {
+                    outEdges = new ArrayList<Edge>();
+                }
+                    outEdges.add(new Edge(fromNode, toNode, weight));
+                    g.put(fromNode, outEdges);
+
+            }
+        }
+        System.out.println("Number of vertices = " + g.keySet().size());
+        System.out.println("Number of edges = " + (edges.length-1));
+
+        prettyPrint();
+
+    }
+
+    public void prettyPrint()
+    {
+        // Get a set of the entries
+        Set set = g.entrySet();
+
+        // Get an iterator
+        Iterator i = set.iterator();
+
+        // Display elements
+        while(i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            System.out.print(me.getKey() + ": [ ");
+            ArrayList<Edge> edges = (ArrayList<Edge>) me.getValue();
+
+            for (int j=0; j<edges.size(); j++)
+            {
+                edges.get(j).prettyPrint();
+                if (j!=edges.size()-1)
+                    System.out.print(", ");
+            }
+
+            System.out.println(" ]");
+
+        }
+        System.out.println();
+
+    }
+
+    public void drawGraph()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        Graph newG = new Graph();
+        newG.makeGraph("world_graph");
+    }
+
+
+}
